22c2289210f6eaa41170ebfd0bda4a8889c286a1
Joanne <jungyoon.lim99@gmail.com>
2024-11-30 17:22:39 +0900

    lul

--- saeum/src/components/welcome/WelcomeCard.tsx
+++ saeum/src/components/welcome/WelcomeCard.tsx
@@ -1,51 +1,47 @@
 /**
  * 
 <div className="w-[1364px] h-[900px] relative bg-white">
-<div className="w-[321px] h-[416px] left-[521px] top-[242px] absolute bg-[#222bb7]/75 shadow" />
-<div className="w-[321px] h-[416px] left-[521px] top-[224px] absolute bg-[#222bb7]/70 shadow" />
-<div className="w-[321px] h-[416px] left-[521px] top-[204px] absolute bg-[#222bb7]/75 shadow" />
-<div className="w-[321px] h-[416px] left-[521px] top-[185px] absolute bg-[#222bb7] shadow" />
-<div className="w-[90px] h-[81px] left-[17px] top-[32px] absolute" />
-<div className="w-[53px] h-[53px] left-[1278px] top-[32px] absolute bg-[#d9d9d9] rounded-full" />
-<div className="w-[441.04px] h-[505.27px] left-[440px] top-[153px] absolute">
-<div className="w-[221px] left-[56px] top-[120.20px] absolute origin-top-left rotate-[-15deg] text-white text-xl font-normal font-['Instrument Sans']">Welcome to<br/>Saeum.</div>
+    <div className="w-[321px] h-[416px] left-[521px] top-[242px] absolute bg-[#222bb7]/75 shadow" />
+    <div className="w-[321px] h-[416px] left-[521px] top-[224px] absolute bg-[#222bb7]/70 shadow" />
+    <div className="w-[321px] h-[416px] left-[521px] top-[204px] absolute bg-[#222bb7]/75 shadow" />
+    <div className="w-[321px] h-[416px] left-[521px] top-[185px] absolute bg-[#222bb7] shadow" />
+    <div className="w-[90px] h-[81px] left-[17px] top-[32px] absolute" />
+    <div className="w-[53px] h-[53px] left-[1278px] top-[32px] absolute bg-[#d9d9d9] rounded-full" />
+    <div className="w-[441.04px] h-[505.27px] left-[440px] top-[153px] absolute">
+        <div className="w-[321px] h-[416px] left-[4px] top-[110.32px] absolute origin-top-left rotate-[-21.82deg] bg-[#333dd7] shadow" />
+        <div className="w-[221px] left-[157px] top-[421.36px] absolute origin-top-left rotate-[-23.28deg] text-white text-xl font-normal font-['Instrument Sans']">Welcome to<br/>Saeum.</div>
+    </div>
 </div>
-</div> 
- * 
  * 
  * 
  * 
  */
 
-const WelcomeCard = ({ secondLine = "Saeum." }) => {
+
+
+
+const WelcomeCard = () => {
     return (
         <div className="w-screen h-screen relative bg-white">
-            {/* Stack of blue cards */}
-            <div className="w-1/4 h-1/2 absolute top-60 left-1/3 bg-[#222bb7] shadow" />
-            <div className="w-1/4 h-1/2 absolute top-52 left-1/3 bg-[#222bb7]/75 shadow" />
-            <div className="w-1/4 h-1/2 absolute top-44 left-1/3 bg-[#222bb7]/70 shadow" />
-            <div className="w-1/4 h-1/2 absolute top-36 left-1/3 bg-[#222bb7]/75 shadow" />
-            
-            
-            {/* Simple text test */}
-            <div className="
-                    absolute 
-                    w-[319px] 
-                    h-[418px] 
-                    left-1/2 
-                    top-1/2 
-                    -translate-x-1/2 
-                    -translate-y-1/2
-                    bg-[#343ED8]  {/* This seems to be the front card color from your screenshot */}
-                    shadow-lg
-                    rotate-[15deg]  {/* Adjust this angle to match */}
-                    z-50
-                ">
-            <div className="w-[221px] origin-top-left rotate-[-15deg] text-white text-xl font-normal font-['Instrument Sans']">Welcome to<br/>Saeum.</div>
-            </div>
+        {/* Stack of blue cards - proportional measurements */}
+        <div className="w-1/4 h-[45%] absolute left-[38%] top-[27%] bg-[#222bb7]/75 shadow" />
+        <div className="w-1/4 h-[45%] absolute left-[38%] top-[25%] bg-[#222bb7]/70 shadow" />
+        <div className="w-1/4 h-[45%] absolute left-[38%] top-[23%] bg-[#222bb7]/75 shadow" />
+        <div className="w-1/4 h-[45%] absolute left-[38%] top-[21%] bg-[#222bb7] shadow" />
+
+        {/* Logo and profile - proportional */}
+        <div className="w-[7%] h-[9%] left-[1%] top-[4%] absolute" />
+        <div className="w-[4%] h-[6%] right-[4%] top-[4%] absolute bg-[#d9d9d9] rounded-full" />
+
+        {/* Text container - proportional */}
+        <div className="w-1/3 h-[56%] left-[32%] top-[17%] absolute">
+        <div className="w-[321px] h-[416px] left-[4px] top-[110.32px] absolute origin-top-left rotate-[-21.82deg] bg-[#333dd7] shadow" />
+        <div className="w-[221px] left-[157px] top-[421.36px] absolute origin-top-left rotate-[-23.28deg] text-white text-xl font-normal font-['Instrument Sans']">
+            Welcome to<br/>Saeum.
         </div>
+        </div>
+    </div>
     )
 }
-
 
 export default WelcomeCard
