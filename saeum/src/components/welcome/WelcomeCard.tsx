42743901b70d131fa2d39cfe1fcd3f9d57896db3
Joanne <jungyoon.lim99@gmail.com>
2024-11-30 13:32:17 +0900

    some init adjustments

--- saeum/src/components/welcome/WelcomeCard.tsx
+++ saeum/src/components/welcome/WelcomeCard.tsx
@@ -1,28 +1,16 @@
-import React from 'react'
-
-
-
-
-/**
- * 
- * 
- * <div className="w-[441.04px] h-[505.27px] relative">
-    <div className="w-[221px] left-[56px] top-[120.20px] absolute origin-top-left rotate-[-15deg] text-white text-xl font-normal font-['Instrument Sans']">
-        Welcome to<br/>Saeum.
-    </div>
-</div>
- * 
- * 
- * 
- * 
- */
-
-const WelcomeCard = () => {
-
+interface WelcomeCardProps {
+    secondLine?: string; 
 }
 
-<div className="flex flex-col relative justify-start items-start">
-    <div className="">
-        Welcome to<br/>Saeum. 
-    </div>
-</div>
+const WelcomeCard = ({ secondLine }: WelcomeCardProps) => {
+    return (
+        <div className="flex flex-col relative justify-start items-start">
+            <div className="text-white text-xl transform -rotate-12">
+                <div>Welcome to</div>
+                <div>{secondLine}</div>
+            </div>
+        </div>
+    )
+}
+
+export default WelcomeCard
--- saeum/src/pages/Welcome.tsx
+++ saeum/src/pages/Welcome.tsx
@@ -1,17 +1,15 @@
 //@ts-ignore
 import React from 'react'
 import WelcomeCard from '../components/welcome/WelcomeCard'
-import WelcomeText from '../components/welcome/WelcomeText'
-import Logo from '../components/welcome/Logo'
+// import WelcomeText from '../components/welcome/WelcomeText'
+// import Logo from '../components/welcome/Logo'
 
 
 const Welcome = () => {
     return (
         <div className="min-h-screen bg-white relative">
-            <Logo />
             <div className="relative">
                 <WelcomeCard />
-                <WelcomeText />
             </div>
         </div>
     )
